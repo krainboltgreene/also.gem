b3b57fd726758f4a8a064483bbfbeb64c176032b
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2015-10-08 11:16:24 -0700

    Run two commands async

--- lib/also.rb
+++ lib/also.rb
@@ -1,3 +1,17 @@
-module Also
+require "subprocess"
+require "celluloid"
+
+class Also
+  include Celluloid
+
+  def initialize(arguments, options)
+    arguments.each do |argument|
+      async.execute(argument.split(" "))
+    end
+  end
+
+  def execute(command)
+    Subprocess.check_call(command)
+  end
   require_relative "also/version"
 end
